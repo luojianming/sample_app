3bb3e3343734e5e00b9bdff80338b8c7c06adf93
luojianming <luojmfly@gmail.com>
2012-11-12 11:50:17 +0800

    Finish layout and routes

--- Gemfile
+++ Gemfile
@@ -2,6 +2,7 @@ source 'https://rubygems.org'
 
 gem 'rails', '3.2.8'
 
+gem 'bootstrap-sass', '2.0.0'
 # Bundle edge Rails instead:
 # gem 'rails', :git => 'git://github.com/rails/rails.git'
 
@@ -32,6 +33,7 @@ end
 group :production do
   gem 'pg', '0.12.2'
 end
+
 
 # To use ActiveModel has_secure_password
 # gem 'bcrypt-ruby', '~> 3.0.0'
--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -1,54 +1,49 @@
 require 'spec_helper'
 
 describe "StaticPages" do
-  let(:base_title) {"Ruby on Rails Tutorial Sample App"}
   describe "Home page" do
+    before { visit root_path }
      it "should have the content 'Sample App'" do
-      visit '/static_pages/home'
       page.should have_content('Sample App')
     end
 
      it "should have the base title" do
-       visit '/static_pages/home'
-       page.should have_selector('title',:text => "Ruby on Rails Tutorial Sample App")
+       page.should have_selector('title', text:  full_title(''))
      end
 
      it "should not have a custom page title" do
-       visit '/static_pages/home'
-       page.should_not have_selector('title', :text => '| Home')
+       page.should_not have_selector('title', :text => full_title('Home'))
      end
   end
 
   describe "Help page" do
+    before { visit help_path }
     it "should have the content 'Help'" do
-      visit '/static_pages/help'
       page.should have_content('Help')
     end
 
      it "should have the right title" do
-       visit '/static_pages/help'
-       page.should have_selector('title',:text => "#{base_title} | Help")
+       page.should have_selector('title',:text => full_title('Help'))
      end
   end
 
   describe "About page" do
+    before { visit about_path }
     it "should have the content 'About Us'" do
-      visit '/static_pages/about'
       page.should have_content('About Us')
     end
 
 
      it "should have the right title" do
-       visit '/static_pages/about'
-       page.should have_selector('title',:text => "#{base_title} | About us")
+       page.should have_selector('title',:text => full_title("About us"))
      end
   end
 
   describe "Contact us" do
+    before { visit contact_path }
     it "should have the right title" do
-      visit '/static_pages/contact'
       page.should have_selector('title',
-                                :text => "#{base_title} | Contact us")
+                                :text => full_title("Contact us"))
     end
   end
 end
