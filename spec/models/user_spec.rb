17cae557a18911187993f5afe8757905c6c5f954
luojianming <luojmfly@gmail.com>
2012-11-16 00:19:19 +0800

    Finish sign in

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -20,6 +20,7 @@ describe User do
     it { should respond_to(:password_digest) }
     it { should respond_to(:password) }
     it { should respond_to(:password_confirmation) }
+    it { should respond_to(:remember_token) }
     it { should be_valid }
 
     describe "when name is not present" do
@@ -32,6 +33,10 @@ describe User do
       it { should_not be_valid }
     end
     
+    describe "remember token" do
+      before { @user.save }
+      its(:remember_token) { should_not be_blank }
+    end
     describe "when password is not present"  do
       before { @user.password = @user.password_confirmation = " " }
       it { should_not be_valid }
--- spec/requests/user_pages_spec.rb
+++ spec/requests/user_pages_spec.rb
@@ -2,6 +2,7 @@ require 'spec_helper'
 
 describe "UserPages" do
   subject { page }
+
   describe "signup page" do
     before { visit signup_path }
 
@@ -37,6 +38,16 @@ describe "UserPages" do
       it "should create a user" do
         expect { click_button submit }.to change(User, :count).by(1)
       end
+
+      describe "after saving the user" do
+        before { click_button submit }
+        let(:user) { User.find_by_email('ljm@gmail.com') }
+
+        it { should have_selector('title', text: user.name) }
+        it { should have_selector('div.alert.alert-success', text: 'Welcome') }
+        it { should have_link('Sign out') }
+      end
     end
+
   end
 end
